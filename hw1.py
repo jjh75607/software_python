d416ca583b0ebe43a15fb8fbc54cd9ad8c04afc7
jjh75607 <dkrak5738@naver.com>
2020-04-21 16:04:01 +0900

    homework first week finish
    과제 첫주 끝

---
+++ hw1.py
@@ -0,0 +1,29 @@
+print(bin(-53))
+print(oct(-53))
+print(hex(-53))
+print("--------")
+
+print(int(0b000100010001))
+print(oct(0b000100010001))
+print(hex(0b000100010001))
+print("--------")
+
+print(int(0o77))
+print(bin(0o77))
+print(hex(0o77))
+print("--------")
+
+print(int(0x9F))
+print(bin(0x9F))
+print(oct(0x9F))
+print("----------")
+
+birthday = '890317'
+year = birthday[:2]
+month = birthday[2:4]
+day = birthday[4:]
+
+print("Birthday is")
+print("Year = ", year)
+print("Month = ", month)
+print("Day = ", day)
